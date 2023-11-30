4a3b9e9e699e96b58afbc4eed21db6943e22dfbf
Saai151 <94809929+Saai151@users.noreply.github.com>
2023-11-30 11:44:17 -0500

    new branch move

--- main.cc
+++ main.cc
@@ -5,4 +5,9 @@ using namespace std;
 int main(){
     Board myBoard;
     cout << myBoard;
+
+    string command;
+    std::cin >> command;
+
+    // work on what happens when the user enters setup mode
 }
