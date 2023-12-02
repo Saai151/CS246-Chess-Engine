62e9cbd658119fe5ff92617cc129bdb5a3a7a891
Saai151 <94809929+Saai151@users.noreply.github.com>
2023-12-02 18:31:17 -0500

    Piece validation for moves is done (not tested)

--- AbstractPiece.cc
+++ AbstractPiece.cc
@@ -1,108 +1,399 @@
 #include "AbstractPiece.h"
 
-AbstractPiece::~AbstractPiece(){
+AbstractPiece::~AbstractPiece()
+{
     pieceRemovedObserver->handlePieceRemoved(this);
 }
 
-void AbstractPiece::move(int newIndex) {
+void AbstractPiece::move(int newIndex)
+{
     previousSquareIndex = squareIndex;
     squareIndex = newIndex;
     pieceMovedObserver->handlePieceMoved(this);
     std::cout << "MOVING" << std::endl;
 }
 
-std::string Pawn::printable() const{
-    if (getPieceColor() == ChessColor::White){
+std::string Pawn::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "P";
     }
-    else {
+    else
+    {
         return "p";
     }
 }
 
-bool Pawn::validMove(int targetSquare, vector<AbstractPiece*> boardState){
-        ChessColor targetColor;
-        if (this->getPieceColor() == ChessColor::White){
-            targetColor = ChessColor::Black;
-        } else{
-                targetColor = ChessColor::White;
-        }
-        int currSquare = this->getSquare();
-        bool occupied1 = false;
-        bool occupied2 = false;
-        std::vector<int> moves;
-        
-        for ( auto piece : boardState) {
-            if (piece->getSquare() == currSquare + 8) {
-                occupied1 = true;
-            }
-            else if (piece->getSquare() == currSquare + 16){
-                occupied2 = true;
-            }
-            else if(piece->getSquare() == currSquare + 9 && piece->getPieceColor() == targetColor){
-                moves.push_back(currSquare + 9);
-            }
-            else if (piece->getSquare() == currSquare - 9 && piece->getPieceColor() == targetColor){
-                moves.push_back(currSquare - 9);
-            }
+bool Pawn::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+    ChessColor targetColor;
+    if (this->getPieceColor() == ChessColor::White)
+    {
+        targetColor = ChessColor::Black;
+    }
+    else
+    {
+        targetColor = ChessColor::White;
+    }
+    int currSquare = this->getSquare();
+    bool occupied1 = false;
+    bool occupied2 = false;
+    std::vector<int> moves;
 
-        if (isFirst && !occupied2){
-            moves.push_back(currSquare+16);
+    for (auto piece : boardState)
+    {
+        if (piece->getSquare() == currSquare + 8)
+        {
+            occupied1 = true;
         }
-        else if (!occupied1){
-            moves.push_back(currSquare+8);
+        else if (piece->getSquare() == currSquare + 16)
+        {
+            occupied2 = true;
+        }
+        else if (piece->getSquare() == currSquare + 9 && piece->getPieceColor() == targetColor)
+        {
+            moves.push_back(currSquare + 9);
+        }
+        else if (piece->getSquare() == currSquare - 9 && piece->getPieceColor() == targetColor)
+        {
+            moves.push_back(currSquare - 9);
         }
 
-        for (int i = 0; i < moves.size(); ++i){
-            if (targetSquare == moves[i]){
+        if (isFirst && !occupied2)
+        {
+            moves.push_back(currSquare + 16);
+        }
+        else if (!occupied1)
+        {
+            moves.push_back(currSquare + 8);
+        }
+
+        for (int i = 0; i < moves.size(); ++i)
+        {
+            if (targetSquare == moves[i])
+            {
                 return true;
             }
         }
-        return false;    
-    }        
+        return false;
+    }
 }
 
-std::string Queen::printable() const  {
-    if (getPieceColor() == ChessColor::White){
+std::string Queen::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "Q";
     }
-    else {
+    else
+    {
         return "q";
     }
 }
 
-std::string King::printable() const {
-    if (getPieceColor() == ChessColor::White){
+bool Queen::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+        int currSquare = this->getSquare();
+
+    vector<int> moves;
+    int i = 8;
+    int j = 8;
+    int a = 1;
+    int b = 1;
+    while (true)
+    {
+        if (i + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            i += i;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - i >= 0)
+        {
+            moves.push_back(currSquare - i);
+            j += j;
+        }
+    }
+    while (true)
+    {
+        if (b + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            b += b;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - a >= 0)
+        {
+            moves.push_back(currSquare - a);
+            a += a;
+        }
+    }
+
+    int currSquare = this->getSquare();
+
+    i = 9;
+    j = 9;
+    a = 7;
+    b = 7;
+    while (true)
+    {
+        if (i + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            i += i;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - i >= 0)
+        {
+            moves.push_back(currSquare - i);
+            j += j;
+        }
+    }
+    while (true)
+    {
+        if (b + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            b += b;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - a >= 0)
+        {
+            moves.push_back(currSquare - a);
+            a += a;
+        }
+    }
+
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+        if (targetSquare == moves[i])
+        {
+            return true;
+        }
+    }
+}
+
+std::string King::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "K";
     }
-    else {
+    else
+    {
         return "k";
     }
 }
 
-std::string Knight::printable() const {
-    if (getPieceColor() == ChessColor::White){
+bool King::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+    int currSquare = this->getSquare();
+    vector<int> moves = {currSquare + 8, currSquare - 8, currSquare + 9, currSquare - 9};
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+        if (targetSquare == moves[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string Knight::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "N";
     }
-    else {
+    else
+    {
         return "n";
     }
 }
 
-std::string Rook::printable() const {
-    if (getPieceColor() == ChessColor::White){
+bool Knight::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+    int currSquare = this->getSquare();
+    vector<int> moves = {currSquare + 17, currSquare + 15, currSquare - 17,
+                         currSquare - 15, currSquare + 10, currSquare - 10, currSquare + 6, currSquare - 6};
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+        if (targetSquare == moves[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string Rook::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "R";
     }
-    else {
+    else
+    {
         return "r";
     }
 }
 
-std::string Bishop::printable() const {
-    if (getPieceColor() == ChessColor::White){
+bool Rook::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+    int currSquare = this->getSquare();
+
+    vector<int> moves;
+    int i = 8;
+    int j = 8;
+    int a = 1;
+    int b = 1;
+    while (true)
+    {
+        if (i + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            i += i;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - i >= 0)
+        {
+            moves.push_back(currSquare - i);
+            j += j;
+        }
+    }
+    while (true)
+    {
+        if (b + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            b += b;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - a >= 0)
+        {
+            moves.push_back(currSquare - a);
+            a += a;
+        }
+    }
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+        if (targetSquare == moves[i])
+        {
+            return true;
+        }
+    }
+}
+
+std::string Bishop::printable() const
+{
+    if (getPieceColor() == ChessColor::White)
+    {
         return "B";
     }
-    else {
+    else
+    {
         return "b";
     }
+}
+
+bool Bishop::validMove(int targetSquare, vector<AbstractPiece *> boardState)
+{
+    int currSquare = this->getSquare();
+
+    vector<int> moves;
+    int i = 9;
+    int j = 9;
+    int a = 7;
+    int b = 7;
+    while (true)
+    {
+        if (i + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            i += i;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - i >= 0)
+        {
+            moves.push_back(currSquare - i);
+            j += j;
+        }
+    }
+    while (true)
+    {
+        if (b + currSquare <= 63)
+        {
+            moves.push_back(i + currSquare);
+            b += b;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true)
+    {
+        if (currSquare - a >= 0)
+        {
+            moves.push_back(currSquare - a);
+            a += a;
+        }
+    }
+
+    for (int i = 0; i < moves.size(); ++i)
+    {
+        if (targetSquare == moves[i])
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
--- AbstractPiece.h
+++ AbstractPiece.h
@@ -12,114 +12,132 @@ using namespace std;
 class PieceRemovedObserver;
 class PieceMovedObserver;
 
-class AbstractPiece {
-    protected:
-        ChessColor color;
+class AbstractPiece
+{
+protected:
+    ChessColor color;
 
-    private:
-        int squareIndex = -1;
-        int previousSquareIndex = -1;
-        std::string name;
-        PieceRemovedObserver* pieceRemovedObserver;
-        PieceMovedObserver* pieceMovedObserver;
+private:
+    int squareIndex = -1;
+    int previousSquareIndex = -1;
+    std::string name;
+    PieceRemovedObserver *pieceRemovedObserver;
+    PieceMovedObserver *pieceMovedObserver;
 
-    public:
-        AbstractPiece(ChessColor color, std::string name, PieceRemovedObserver* pieceRemovedObserver) : 
-            color{color}, name{name}, pieceRemovedObserver{pieceRemovedObserver} {}
+public:
+    AbstractPiece(ChessColor color, std::string name, PieceRemovedObserver *pieceRemovedObserver) : color{color}, name{name}, pieceRemovedObserver{pieceRemovedObserver} {}
 
-        ChessColor getPieceColor() const{
-            return color;
-        }
+    ChessColor getPieceColor() const
+    {
+        return color;
+    }
 
-        std::string getName(){
-            return name;
-        }
+    std::string getName()
+    {
+        return name;
+    }
 
-        void attachMoveObserver(PieceMovedObserver* board) {
-            pieceMovedObserver = board;
-        }
+    void attachMoveObserver(PieceMovedObserver *board)
+    {
+        pieceMovedObserver = board;
+    }
 
-        int getSquare() {
-            return squareIndex;
-        }
+    int getSquare()
+    {
+        return squareIndex;
+    }
 
-        int getPreviousSquare() {
-            return previousSquareIndex;
-        }
+    int getPreviousSquare()
+    {
+        return previousSquareIndex;
+    }
 
-        void setSquare(int index){
-            squareIndex = index;
-        }
+    void setSquare(int index)
+    {
+        squareIndex = index;
+    }
 
-        virtual std::string printable() const = 0;
-        virtual void move(int newIndex);
-        virtual ~AbstractPiece();
-        virtual bool validMove(int targetSquare, vector<AbstractPiece*> boardState);
+    virtual std::string printable() const = 0;
+    virtual void move(int newIndex);
+    virtual ~AbstractPiece();
 };
 
-class PieceRemovedObserver {
-    public:
-        virtual void handlePieceRemoved(AbstractPiece* p) = 0;
+class PieceRemovedObserver
+{
+public:
+    virtual void handlePieceRemoved(AbstractPiece *p) = 0;
 };
 
-class PieceMovedObserver {
-    public:
-        virtual void handlePieceMoved(AbstractPiece* p) = 0;
+class PieceMovedObserver
+{
+public:
+    virtual void handlePieceMoved(AbstractPiece *p) = 0;
 };
 
-
-class Pawn : public AbstractPiece{
+class Pawn : public AbstractPiece
+{
     bool isFirst;
-    public:
-        Pawn(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-            AbstractPiece(color, "Pawn", pieceRemovedObserver) {}
-        
-        std::string printable() const override;
 
-        std::vector<Location> getValidMoves(){
-        }
+public:
+    Pawn(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "Pawn", pieceRemovedObserver) {}
 
-        bool validMove(int targetSquare, vector<AbstractPiece*> boardState);
+    std::string printable() const override;
+
+    std::vector<Location> getValidMoves()
+    {
+    }
+
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);
 };
 
-class Queen : public AbstractPiece{
-    public:
-        Queen(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-                    AbstractPiece(color, "Queen", pieceRemovedObserver) {}
+class Queen : public AbstractPiece
+{
+public:
+    Queen(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "Queen", pieceRemovedObserver) {}
 
-        std::string printable() const override;
+    std::string printable() const override;
+
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);
 };
 
-class King : public AbstractPiece{
-    public:
-        King(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-            AbstractPiece(color, "King", pieceRemovedObserver) {}
+class King : public AbstractPiece
+{
+public:
+    King(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "King", pieceRemovedObserver) {}
 
-        std::string printable() const override;
+    std::string printable() const override;
+
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);    
 };
 
-class Knight : public AbstractPiece{
-    public:
-        Knight(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-            AbstractPiece(color, "Knight", pieceRemovedObserver) {}
-        
-        std::string printable() const override;
+class Knight : public AbstractPiece
+{
+public:
+    Knight(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "Knight", pieceRemovedObserver) {}
+
+    std::string printable() const override;
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);
+
 };
 
-class Rook : public AbstractPiece{
-    public:
-        Rook(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-            AbstractPiece(color, "Rook", pieceRemovedObserver) {}
+class Rook : public AbstractPiece
+{
+public:
+    Rook(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "Rook", pieceRemovedObserver) {}
 
-        std::string printable() const override;
+    std::string printable() const override;
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);
+
 };
 
-class Bishop : public AbstractPiece{
-    public:
-        Bishop(ChessColor color, PieceRemovedObserver* pieceRemovedObserver) : 
-            AbstractPiece(color, "Bishop", pieceRemovedObserver) {}
+class Bishop : public AbstractPiece
+{
+public:
+    Bishop(ChessColor color, PieceRemovedObserver *pieceRemovedObserver) : AbstractPiece(color, "Bishop", pieceRemovedObserver) {}
 
-        std::string printable() const override;
+    std::string printable() const override;
+    bool validMove(int targetSquare, vector<AbstractPiece *> boardState);
+
 };
 
 #endif
